613fc98dc2b99cdaf4572c21f87a05e9f59af0c2
Caio <caio1@LAPTOP-EE98JVOS>
2022-04-02 22:52:56 -0300

    Organizando main

--- Task2022.03.31/task01.py
+++ Task2022.03.31/task01.py
@@ -32,11 +32,10 @@ def insertionSortMetric(vetor):
     for i in range(1, len(vetor)):
         aux = vetor[i]
         j = i - 1 
-        count += 1  
         while j >= 0 and aux < vetor[j]:
             vetor[j + 1] = vetor[j]
             j = j - 1
-            
+            count += 1
         vetor[j + 1] = aux
     return count
 
@@ -51,32 +50,41 @@ def shellSortMetric(vetor):
         for i in range(h, n, h):
             atual = vetor[i]
             j = i - h
-            count += 1
             while j >= 0 and atual < vetor[j]:
                 vetor[j + h] = vetor[j]
                 j = j - h
+                count += 1
             vetor[j + h ] = atual
     return count
 
 # Código principal
 
 import random
+import os
 
 vetor = []
 
 while True:
-    print('Vetor ordenado: ', vetor)
-    vetor = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    #vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    print('Vetor desrodenado: ', vetor)
+    vetorType = (int(input('1 - Vetor ordenado\n2 - Vetor desordenado\nSelecione uma opção: ')))
+    os.system('cls')
+    if vetorType == 1:
+        vetorOrder = 'Ordenado'
+        vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    else:
+        vetorOrder = 'Desordenado'
+        vetor = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     #for i in range(0, 10):
     #    vetor.append(random.randint(1,52))
-    menu = int(input('''Escolha o método de ordenação:\n1 - BubbleSort\n2 - Insertion Sort\n3 - Selection Sort\n4 - Shell Sort\n\n'''))
+    menu = int(input(f'''Escolha o método de ordenação:\n1 - BubbleSort\n2 - Insertion Sort\n3 - Selection Sort\n4 - Shell Sort\nSelecione uma opção: ({vetorOrder}) \n'''))
     if menu == 1:
-        print('\n\nBubbleSort - Rodou: ', bubbleSortMetric(vetor), '\n\n')
+        os.system('cls')
+        print(f'BubbleSort - Quantidade de operações realizadas: {bubbleSortMetric(vetor)} - ({vetorOrder})\nVetor ordenado: {vetor}\n')
     elif menu == 2:
-        print('\n\nSelection Sort - Rodou: ', selectionSortMetric(vetor), '\n\n')
+        os.system('cls')
+        print(f'Selection Sort - Quantidade de operações realizadas: {selectionSortMetric(vetor)} - ({vetorOrder})\nVetor ordenado: {vetor}\n')
     elif menu == 3:
-        print('\n\nInsertion Sort - Rodou: ', insertionSortMetric(vetor), '\n\n')
+        os.system('cls')
+        print(f'Insertion Sort - Quantidade de operações realizadas: {insertionSortMetric(vetor)} - ({vetorOrder})\n\Vetor ordenado: {vetor}n')
     elif menu == 4:
-        print('\n\nShell Sort - Rodou: ', shellSortMetric(vetor), '\n\n')
+        os.system('cls')
+        print(f'Shell Sort - Quantidade de operações realizadas: {shellSortMetric(vetor)} - ({vetorOrder})\nVetor ordenado: {vetor}\n')
